ce4ba6efb24139716e6fb65eb84c0618d06e7712
nel-nelia <nelia_escalante_m@hotmail.com>
2020-06-24 15:25:26 -0500

    colores
    diseño de colores

--- Frontend/ServiceOK/src/views/Maps/Maps.js
+++ Frontend/ServiceOK/src/views/Maps/Maps.js
@@ -1,4 +1,19 @@
 import React from "react";
+//para colocar la cabecera
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+// core components
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from "components/CustomButtons/Button.js";
+
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardAvatar from "components/Card/CardAvatar.js";
+import CardBody from "components/Card/CardBody.js";
+import CardFooter from "components/Card/CardFooter.js";
+//si no funciona borrarlo
 import {
   withScriptjs,
   withGoogleMap,
@@ -6,8 +21,129 @@ import {
   Marker,
 } from "react-google-maps";
 
-
-const CustomSkinMap = withScriptjs(
+//para el diseño
+const styles = {
+  cardCategoryWhite: {
+    "&,& a,& a:hover,& a:focus" : {
+    color: "rgba(255,255,255,.62)",
+    margin: "0",
+    fontSize: "14px",
+    marginTop: "0",
+    marginBottom: "0"
+  },
+  "& a,& a:hover,& a:focus": {
+    color: "#FFFFFF"
+  },
+},
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: "#777",
+      fontSize: "65%",
+      fontWeight: "400",
+      lineHeight: "1"
+    }
+  },
+  tableUpgradeWrapper: {
+    display: "block",
+    width: "100%",
+    overflowX: "auto",
+    WebkitOverflowScrolling: "touch",
+    MsOverflowStyle: "-ms-autohiding-scrollbar"
+  },
+  table: {
+    width: "100%",
+    maxWidth: "100%",
+    marginBottom: "1rem",
+    backgroundColor: "transparent",
+    borderCollapse: "collapse",
+    display: "table",
+    borderSpacing: "2px",
+    borderColor: "grey",
+    "& thdead tr th": {
+      fontSize: "1.063rem",
+      padding: "12px 8px",
+      verticalAlign: "middle",
+      fontWeight: "300",
+      borderTopWidth: "0",
+      borderBottom: "1px solid rgba(0, 0, 0, 0.06)",
+      textAlign: "inherit"
+    },
+    "& tbody tr td": {
+      padding: "12px 8px",
+      verticalAlign: "middle",
+      borderTop: "1px solid rgba(0, 0, 0, 0.06)"
+    },
+    "& td, & th": {
+      display: "table-cell"
+    }
+  },
+  center: {
+    textAlign: "center"
+  }
+};
+//borrarlos en todo caso
+const useStyles = makeStyles(styles);
+//
+//
+export default function TypographyPage() {
+  const classes = useStyles();
+  return (
+    <div>
+    <GridContainer>
+      <GridItem xs={12} sm={12} md={8}>
+        <Card>
+          <CardHeader color="primary">
+            <h4 className={classes.cardTitleWhite}>Gestionar Ubicacion de Establecimiento</h4>
+            <p className={classes.cardCategoryWhite}>
+              Gestionar tus establecimientos
+            </p>
+          </CardHeader>
+         <CardBody>
+         <GridContainer>
+                <GridItem xs={12} sm={12} md={5}>
+                  <CustomInput
+                    labelText="Latitud"
+                    id="lati_establecimiento"
+                    formControlProps={{
+                      fullWidth: true
+                    }}
+                  />
+                </GridItem>
+                <GridItem xs={12} sm={12} md={3}>
+                  <CustomInput
+                    labelText="Longitud"
+                    id="long_establecimiento"
+                    formControlProps={{
+                      fullWidth: true
+                    }}
+                  />
+                </GridItem>
+                
+                </GridContainer>  
+                <CardFooter>
+              <Button color="primary">Aceptar</Button>
+            </CardFooter> 
+         </CardBody>
+        </Card>
+      </GridItem>
+      <GridContainer>
+        
+      </GridContainer>
+     
+    </GridContainer>
+    </div>
+  );
+}
+//
+//script del mapa
+/*const CustomSkinMap = withScriptjs(
   withGoogleMap(() => (
     <GoogleMap
       defaultZoom={13}
@@ -96,4 +232,4 @@ export default function Maps() {
      mapElement={<div style={{ height: `100%` }} />}
     />
   );
-}
+}*/
