f8e57e23d1fef789ef0b14c2c3c1b313f6f51e5d
GiselaF13 <48686385+GiselaF13@users.noreply.github.com>
2020-07-05 22:17:34 -0500

    Datos-Actividad

---
+++ Frontend/ServiceOK/src/Datos/Actividades.js
@@ -0,0 +1,40 @@
+
+{
+    [
+   
+
+    {
+        "actividadTipoId": 8,
+        "nombre": "Gastronomia",
+        "flagActivo": 1
+        
+    },
+
+    {
+        "actividadTipoId": 9,
+        "nombre": "Turismo",
+        "flagActivo": 1
+       
+    },
+
+    {
+        "actividadTipoId": 10,
+        "nombre": "Libreria",
+        "flagActivo": 2
+        
+    },
+    {
+        "actividadTipoId": 11,
+        "nombre": "Centros Comerciales",
+        "flagActivo": 1
+        
+    },
+    {
+        "actividadTipoId": 12,
+        "nombre": "Peluquerias",
+        "flagActivo": 1
+        
+    }
+
+    ]
+}
