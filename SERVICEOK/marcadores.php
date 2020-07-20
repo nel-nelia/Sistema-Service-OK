fd88278382a41bc16ae440d14d73dd573e0c05e8
nel-nelia <nelia_escalante_m@hotmail.com>
2020-07-20 16:03:48 -0500

    mapa
    mapa

--- SERVICEOK/marcadores.php
+++ SERVICEOK/marcadores.php
@@ -4,10 +4,10 @@
   include('conexion.php');
 
   // Listamos las direcciones con todos sus datos (lat, lng, dirección, etc.)
-  $query = mysqli_query($sql, "SELECT * FROM Establecimiento");
+  $result = mysqli_query($sql, "SELECT * FROM Establecimiento");
 
   // Seleccionamos los datos para crear los marcadores en el Mapa de Google serian direccion, lat y lng 
-  while ($row = mysqli_fetch_array($query)) {
-      echo '["' . $row['whatsappurl'] . '", ' . $row['cordenadasx'] . ', ' . $row['cordenadasy'] . '],';
+  while ($row = mysqli_fetch_array($result)) {
+      echo '["' . $row['Nombre'] . '", ' . $row['CordenadasX'] . ', ' . $row['CordenadasY'] . '],';
   }
 ?>
