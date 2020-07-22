df36b95465177ad4c34d2cf044b7577957787377
GiselaF13 <48686385+GiselaF13@users.noreply.github.com>
2020-07-22 16:32:33 -0500

    cambios

--- SERVICEOK/index.php
+++ SERVICEOK/index.php
@@ -10,11 +10,8 @@
 
     <meta name="theme-color" content="#000000" />
 
-    <title>SERVICE OK </title>
+    <title>Cargar una ubicación desde la Base de Datos MySQL con PHP y Google Maps Javascript API </title>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <style type="text/css">
@@ -41,13 +38,13 @@
         <div class="collapse navbar-collapse" id="navbarsExample07">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-            <a class="nav-link" href="http://nubecolectiva.com"> <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="http://nubecolectiva.com">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="http://blog.nubecolectiva.com" target="_blank"></a>
+            <a class="nav-link" href="http://blog.nubecolectiva.com" target="_blank">Blog</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#"></a>
+            <a class="nav-link" href="#">Contacto</a>
           </li> 
           </ul>
           <form name="bencc" method="get" action="http://www.google.com/search" id="bencc" class="form-inline mt-2 mt-md-0" target="_blank">
@@ -85,9 +82,15 @@
 
             <div class="col-md-12">
 
-              <h1 class="mb-4">Service ok </h1>
+              <h1 class="mb-4">Cargar una ubicación desde la Base de Datos MySQL con PHP y Google Maps Javascript API </h1>
 
-               <!-- Contenedor del Mapa de Google --> 
+              <p>"Coloca el puntero del Mouse en cada marcador rojo del Mapa para ver la dirección correspondiente"</p>
+
+              <p>
+                <strong>Nota:</strong><br>
+                El mapa, marcadores y direcciones pueden dar errores de visualización por restricciones de haber excedido la quota diaria gratis de la API de Google Maps Javascript </p>
+
+              <!-- Contenedor del Mapa de Google --> 
               <div id="mapa"></div>               
 
             </div>
@@ -101,7 +104,7 @@
               <h2 class="h2s">Direcciónes en la Base de Datos MySQL</h2>
 
               <!-- Archivo PHP con la lógica para mostrar una tabla con las ubicaciones -->
-              <?php include('app.php'); ?> 
+              <?php include('php/app.php'); ?> 
               
             </div>
             
@@ -110,12 +113,58 @@
 
           <hr>
 
+          <div class="row">
+
+            <div class="col-md-12">
+
+              <p class="lead">En <a href="http://nubecolectiva.com/" target="_blank"> Nube Colectiva </a> hablamos sobre:</p>
+
+            </div>
+
+          </div>
+
+          <div class="row">
+
+              <div class="col-md-3">
+                <h3>Frontend</h3>
+                <a href="http://blog.nubecolectiva.com/category/frontend/" target="_blank">
+                  <img class="img-fluid" src="http://blog.nubecolectiva.com/wp-content/uploads/2018/11/img_destacada_blog_devs-11-300x169.png">
+                </a>
+              </div>
+
+              <div class="col-md-3">
+                <h3>Backend</h3>                
+                <a href="http://blog.nubecolectiva.com/category/backend/" target="_blank">
+                  <img class="img-fluid" src="http://blog.nubecolectiva.com/wp-content/uploads/2018/11/img_destacada_blog_devs-8-300x169.png">
+                </a>
+              </div>              
+
+              <div class="col-md-3">
+                <h3>Android</h3>
+                <a href="http://blog.nubecolectiva.com/category/android/" target="_blank">
+                  <img class="img-fluid" src="http://blog.nubecolectiva.com/wp-content/uploads/2018/11/img_destacada_blog_devs-9-300x169.png">
+                </a>
+              </div>
+
+              <div class="col-md-3">
+                <h3>Otros</h3>
+                <a href="http://blog.nubecolectiva.com/category/articulos/" target="_blank">
+                  <img class="img-fluid" src="http://blog.nubecolectiva.com/wp-content/uploads/2018/09/edit_img_destacada_blog_devs-300x169.png">
+                </a>
+              </div>
+
+          </div>           
+          
+          
+        </div>
 
     </main>
 
 
     <footer class="text-muted mt-3 mb-3">
-       
+        <div align="center">
+          Desarrollado por <a href="http://www.nubecolectiva.com" target="_blank">Nube Colectiva</a>
+      </div> 
     </footer>    
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -141,12 +190,12 @@
 
           // Crear múltiples marcadores desde la Base de Datos 
           var marcadores = [
-              <?php include('marcadores.php'); ?>
+              <?php include('php/marcadores.php'); ?>
           ];
 
           // Creamos la ventana de información para cada Marcador
           var ventanaInfo = [
-              <?php include('info_marcadores.php'); ?>
+              <?php include('php/info_marcadores.php'); ?>
           ];
 
           // Creamos la ventana de información con los marcadores 
