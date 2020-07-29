e4e626ca1bf043162314dc4e4e09656db8684968
nel-nelia <nelia_escalante_m@hotmail.com>
2020-07-29 18:00:29 -0500

    Sistema Final
    Diseño del sistema final xd, ojala con fe

---
+++ serviceo_ok/views/template.php
@@ -0,0 +1,210 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<head>
+<title>Service OK</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Visitors Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- bootstrap-css -->
+<link rel="stylesheet" href="views/css/bootstrap.min.css" >
+<!-- //bootstrap-css -->
+<!-- Custom CSS -->
+<link href="views/css/style.css" rel='stylesheet' type='text/css' />
+<link href="views/css/style-responsive.css" rel="stylesheet"/>
+<!-- font CSS -->
+<link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<!-- font-awesome icons -->
+<link rel="stylesheet" href="css/font.css" type="text/css"/>
+<link href="views/css/font-awesome.css" rel="stylesheet"> 
+<link rel="stylesheet" href="views/css/morris.css" type="text/css"/>
+<!-- calendar -->
+<link rel="stylesheet" href="views/css/monthly.css">
+<!-- //calendar -->
+<!-- //font-awesome icons -->
+<script src="views/js/jquery2.0.3.min.js"></script>
+<script src="views/js/raphael-min.js"></script>
+<script src="views/js/morris.js"></script>
+</head>
+<body>
+<section id="container">
+<!--header start-->
+<header class="header fixed-top clearfix">
+<!--logo start-->
+<div class="brand">
+    <a href="index.html" class="logo">
+        SERVICE OK
+    </a>
+    <div class="sidebar-toggle-box">
+        <div class="fa fa-bars"></div>
+    </div>
+</div>
+<!--logo end-->
+<div class="nav notify-row" id="top_menu">
+    <!--  notification start -->
+    <ul class="nav top-menu">
+        
+        <!-- notification dropdown end -->
+    </ul>
+    <!--  notification end -->
+</div>
+<div class="top-nav clearfix">
+    <!--search & user info start-->
+    <ul class="nav pull-right top-menu">
+        <li>
+            <input type="text" class="form-control search" placeholder=" Search">
+        </li>
+        <!-- user login dropdown start-->
+        <li class="dropdown">
+            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                <img alt="" src="images/2.png">
+                <span class="username">John Doe</span>
+                <b class="caret"></b>
+            </a>
+            <ul class="dropdown-menu extended logout">
+                <li><a href="#"><i class=" fa fa-suitcase"></i>Profile</a></li>
+                <li><a href="#"><i class="fa fa-cog"></i> Settings</a></li>
+                <li><a href="views/login.php"><i class="fa fa-key"></i> Log Out</a></li>
+            </ul>
+        </li>
+        <!-- user login dropdown end -->
+       
+    </ul>
+    <!--search & user info end-->
+</div>
+</header>
+<!--header end-->
+<!--sidebar start-->
+<aside>
+    <div id="sidebar" class="nav-collapse">
+        <!-- sidebar menu start-->
+        <div class="leftside-navigation">
+            <ul class="sidebar-menu" id="nav-accordion">
+                <li>
+                    <a class="active" href="views/registration.php">
+                        <i class="fa fa-dashboard"></i>
+                        <span>Registrate Aqui</span>
+                    </a>
+                </li>
+                
+                
+                <li class="sub-menu">
+                    <a href="javascript:;">
+                        <i class="fa fa-bullhorn"></i>
+                        <span>Sé el servicio más seguro</span>
+                    </a>
+                        
+                    <ul class="sub">
+                        <li><a href="basic_table.html">Manual de Bioseguridad</a></li>
+                        <li><a href="responsive_table.html">Cuida a tu servicio</a></li>
+                    </ul>
+                   
+                </li>
+
+                
+                
+                <li class="sub-menu">
+                    <a href="javascript:;">
+                        <i class="fa fa-envelope"></i>
+                        <span>Preguntas Frecuentes</span>
+                    </a>
+                    
+                </li>
+                          
+                <li class="sub-menu">
+                    <a href="javascript:;">
+                        <i class="fa fa-glass"></i>
+                        <span>Extra</span>
+                    </a>
+                    <ul class="sub">
+                        <li><a href="gallery.html">Gallery</a></li>
+						<li><a href="404.html">404 Error</a></li>
+                        <li><a href="registration.html">Registration</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="views/login.php">
+                        <i class="fa fa-user"></i>
+                        <span>Login Page</span>
+                    </a>
+                </li>
+            </ul>            </div>
+        <!-- sidebar menu end-->
+    </div>
+</aside>
+<!--sidebar end-->
+<!--main content start-->
+<section id="main-content">
+	<section class="wrapper">
+		
+		
+		<div class="agil-info-calendar">
+		<!-- calendar -->
+		<main class="mdl-layout__content mdl-color--grey-100">
+
+        <!-- Map widget-->
+        <main class="mdl-layout__content mdl-color--grey-100">
+         <div class="mdl-cell mdl-cell--6-col-desktop mdl-cell--8-col-tablet mdl-cell--4-col-phone">
+            <div class="mdl-card mdl-shadow--2dp map">
+                <div class="curtain"></div>
+                <div class="mdl-card--expand map__window">
+
+                </div>
+                <div class="mdl-card__supporting-text">
+                    <div class="pull-right">
+                        <div class="map__target-image">
+                            <div class="map__target-picture"></div>
+                        </div>
+                    </div>
+                    <div class="map__target-info">
+                        <h5 class="map__target-title"></h5>
+                        <div class="map__target-location"></div>
+                        <div class="map__target-opening-hours"></div>
+                        <div class="map__target-description"></div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </main>
+    </main>
+		<!-- //tasks -->
+		<div class="agileits-w3layouts-stats">
+					<div class="col-md-4 stats-info widget">
+						
+					</div>
+					<div class="col-md-8 stats-info stats-last widget-shadow">
+						
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+</section>
+ <!-- footer -->
+		  <div class="footer">
+			<div class="wthree-copyright">
+			  <p>© 2020 Service OK. All rights reserved | Design by <a>C II</a></p>
+			</div>
+		  </div>
+  <!-- / footer -->
+</section>
+<!--main content end-->
+</section>
+<script src="views/js/bootstrap.js"></script>
+<script src="views/js/jquery.dcjqaccordion.2.7.js"></script>
+<script src="views/js/scripts.js"></script>
+<script src="views/js/jquery.slimscroll.js"></script>
+<script src="views/js/jquery.nicescroll.js"></script>
+<!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/flot-chart/excanvas.min.js"></script><![endif]-->
+<script src="views/js/jquery.scrollTo.js"></script>
+<!-- morris JavaScript -->	
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBW0td9v69m95iy5Q2YiMebpIO7ztCnuPU&language=en&callback=initMap" async defer></script>
+
+
+</body>
+</html>
