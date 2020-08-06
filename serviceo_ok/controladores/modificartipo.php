eb767bf3234d7a5a6aa6966474f0ee48e0a635c4
GiselaF13 <48686385+GiselaF13@users.noreply.github.com>
2020-08-06 18:40:36 -0500

    modificar tipo actividad

--- serviceo_ok/controladores/modificartipo.php
+++ serviceo_ok/controladores/modificartipo.php
@@ -2,14 +2,14 @@
 include 'conexion.php';
 
 $ActividadID=$_POST['id'];
-
-$nombre=strtoupper($_POST['Nombre']);
+$nombre=$_POST['Nombre'];
 $flagactivo=$_POST['FlagActivo'];
 
-$sql= "UPDATE ActividadTipo SET Nombre='$nombre', FlagActivo='$flagactivo' WHERE ActividadTipoID='$ActividadTipoID'";
+$sql= "UPDATE actividadtipo SET Nombre='$nombre',FlagActivo='$flagactivo' WHERE ActividadTipoID='$ActividadID'";
 
 $result=mysqli_query($con,$sql);
 
 header('Location: ../vistas/listartipo.php');
-mysqli_close($con);  
+mysqli_close($con); 
+ 
 ?>
